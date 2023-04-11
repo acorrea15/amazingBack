3a94d348555936d633873fe952ff4f04d0ddf684
Alfredo A. Correa <105443143+acorrea15@users.noreply.github.com>
2023-04-11 18:53:51 -0300

    Pedido de cambio-Se agrega professional al turno

--- rutas/turnos.rutas.js
+++ rutas/turnos.rutas.js
@@ -36,7 +36,7 @@ async function enviarMail(name, lastName, email, phone, professional, appointmen
       subject: `TURNO CONFIRMADO!`, // Subject line
        
       html: `<h5>Hola, ${name}! </h5>
-             <p>Tu turno para el día ${appointmentDay} de ${appointmentServiceId}${appointmentServiceId !== 'Alisado de cejas' ? ' con Lulú/Martu ' : ' ' }a las ${appointmentHour} hs ya está confirmado.<p/>
+             <p>Tu turno para el día ${appointmentDay} de ${appointmentServiceId}${appointmentServiceId !== 'Alisado de cejas' ? ` con  ${professional==="0" ? "Lulú" : "Martu"}`  : ' ' } a las ${appointmentHour} hs ya está confirmado.<p/>
              <p> <p/>
              <p>En caso de no poder asistir, por favor cancela el turno <a href="https://amazinglook.com.ar/appointment/${id_turnos}/${appointmentServiceId}/cancel">tocando aquí</a> <p/>
              <p> <p/>
